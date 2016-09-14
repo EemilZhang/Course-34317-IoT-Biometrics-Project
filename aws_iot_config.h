594dce077a258094ee40e747f4fa6dc176f28820
EemilZhang <eemilz@me.com>
2016-09-14 16:41:49 +0200

    Add files via upload

---
+++ aws_iot_config.h
@@ -0,0 +1,12 @@
+#ifndef config_usr_h
+#define config_usr_h
+
+//===============================================================
+#define AWS_IOT_MQTT_HOST "a2rcd1k48ok85g.iot.us-west-2.amazonaws.com" 	// Endpoint.
+#define AWS_IOT_MQTT_PORT 443									// Port.
+#define AWS_IOT_CLIENT_ID	"ArduinoYunEemil"						// Client ID.
+#define AWS_IOT_MY_THING_NAME "ArduinoYunEemil"     // Thing name.
+#define AWS_IOT_ROOT_CA_PATH "/root/AWS-IoT-Python-Runtime/certs/rootCA.crt" // Root CA file path.
+//===============================================================
+
+#endif
